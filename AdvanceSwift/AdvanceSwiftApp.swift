1b373e8923a03cc7fd7cc28bbd1e359e4077a71a
Gina Mullins <LittlePeculiar13@gmail.com>
2024-07-03 13:16:25 -0700

    Initial Commit

---
+++ AdvanceSwift/AdvanceSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceSwiftApp.swift
+//  AdvanceSwift
+//
+//  Created by Gina Mullins on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
